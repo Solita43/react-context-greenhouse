ed40eb24252ce8179835bd4e48f5531ae3094c40
Melinda Cortez <meli.matrix@outlook.com>
2023-04-27 13:55:53 -0700

    completed phase 3

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,23 @@
+import React, { useContext } from 'react';
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+export const ClimateContext = React.createContext();
+
+export const useClimate = () => React.useContext(ClimateContext);
+
+function ClimateProvider(props) {
+    const [temperature, setTemperature] = React.useState(50);
+
+    return (
+        <ClimateContext.Provider value={{temperature, setTemperature}}>
+            {props.children}
+        </ClimateContext.Provider>
+    );
+
+};
+
+export default ClimateProvider; // Could also add export default in front of line 10.
+
+// export {ClimateContext, useClimate};
